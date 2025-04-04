fdc895d302113cfa6645544a6d228caab35c3b63
aliko <a.minyukovich@gmail.com>
2025-04-04 14:21:52 +0200

    Revert "added Counter component"
    This reverts commit 7135aae94b417dce6679d3064a99bd1fddd9510f.

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,25 +1,12 @@
-import { useState } from "react";
 import '../css/App.css';
 import Board from "./Board";
-import Counter from "./Counter";
-// NOTE: we don't have to use context in this case (Counter and Board aren't too far away from each other) - we only use it for practice
-import { GameContext, gameStates } from "../contexts/GameContext";
 
 export default function App() {
-  // NOTE how we link the state variable below to context in here: <GameContext value={{gameState, setGameState}}>
-  const [ gameState, setGameState ] = useState({
-    nFlips: 0,
-    state: gameStates.notStarted
-  });
-
   return (
     <div className="main-container">
       <div className="centered-item">
         <h1>Find Matching Cards If You Can!</h1>
-        <GameContext value={{gameState, setGameState}}>
-          <Counter/>
-          <Board/>
-        </GameContext>
+        <Board/>
       </div>
     </div>
   );
