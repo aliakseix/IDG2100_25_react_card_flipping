56434b5a45d8e9accf32a20449ebe6a31093e47e
aliko <a.minyukovich@gmail.com>
2025-04-04 14:54:45 +0200

    Revert "added integration test examples for components"
    This reverts commit 988b16b324e24878543a25836aeb40db3b8caaea.

--- src/jsx/Card.jsx
+++ src/jsx/Card.jsx
@@ -5,8 +5,8 @@ import backfaceImgUrl from "/card_backface.jpg";
 export default function Card(props){
 	return (
 		<div onClick={props.onClick} className="card-container">
-			<div data-testid="card-front-face" className={"card card-frontface " + (props.flipped?"":"flipped") }>
-				<img data-testid="card-front-face-image" className="card-image" src={"/icons/" + props.fname} />
+			<div className={"card card-frontface " + (props.flipped?"":"flipped") }>
+				<img className="card-image" src={"/icons/" + props.fname} />
 				<p className="card-text">{props.name}</p>
 			</div>
 			<div className={"card card-backface " +  (props.flipped?"flipped":"")} style={{backgroundImage: 'url(' + backfaceImgUrl + ')'}}></div>
